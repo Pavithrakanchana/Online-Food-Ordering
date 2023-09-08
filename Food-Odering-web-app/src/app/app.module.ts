9edda81d4156edb0e9c745bd75593007a20b793b
pavi0405 <74247443+Pavithrakanchana@users.noreply.github.com>
2023-09-08 21:02:35 +0530

    created required components

--- Food-Odering-web-app/src/app/app.module.ts
+++ Food-Odering-web-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { UsersComponent } from './components/users/users.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    UsersComponent,
+    MenuComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
